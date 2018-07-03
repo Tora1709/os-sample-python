3abd72a2d275e9fa9675bc044a3aa4c6e73c5071
Kevin <kevin@incompany.cr>
2018-07-03 17:50:35 -0600

    varios

---
+++ flask_app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+
+#WA[
+from whatsapp import Client
+expected_token = '09fcWoxQcaD1VT0tejvIGZSZ0To='
+#]WA
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello World!'
+
+@app.route('/msg', methods = ['POST'])
+def msg():
+    to = request.form['to']
+    return str(to)
+
+@app.route('/sendmsg')
+def sendmsg():
+    to = request.args.get('to')
+    msg = request.args.get('msg')
+    token = request.args.get('token')
+    if(str(token) == expected_token):
+        client = Client(login='50671540953', password='qldAt6KaPtsTRLZZk9KA/m/G4wM=')
+        res = client.send_message(to, msg)  
+    
+    else:
+        res = 'Unauthorized'
+    
+    return str(res)
+
+if __name__ == '__main__':
+    app.run()
