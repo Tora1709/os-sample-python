3c94595467aca497f173d09567a609a15e5eeda8
Kevin <kevin@incompany.cr>
2018-07-03 18:33:10 -0600

    as

--- config.py
+++ config.py
@@ -6,16 +6,13 @@ threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+#virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 
 
 from flaskapp import app as application
-if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-    ip = os.environ['OPENSHIFT_PYTHON_IP']
-    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-    #host_name = os.environ['OPENSHIFT_GEAR_DNS']
-    httpd = make_server(ip, 8000, application)
-    #httpd = make_server('localhost', 8051, application)
-    # Wait for a single request, serve it and quit.
-    httpd.handle_request()
-    #httpd.serve_forever()
